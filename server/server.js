15dde5ddf038d8e08281f808a20d40fb9ee049e7
Jonathan <jonathan.y.cai@outlook.com>
2024-10-05 12:59:58 -0700

    Implemented basic server

--- server/server.js
+++ server/server.js
@@ -4,3 +4,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('<h1>Hello, Express.js Server!')
 })
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
